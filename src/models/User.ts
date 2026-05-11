4d7f76e4e7b58d0f478d4369f05021a0ee15b2fa
Raaghav <thakurraaghav2002@gmail.com>
2026-05-11 23:47:34 +0530

    removing resumeText

--- src/models/User.ts
+++ src/models/User.ts
@@ -25,8 +25,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    resumeText: {type: String, default: ""},
-    interviews: [InterviewSchema] 
+    interviews: [InterviewSchema]
 });
 
 export default mongoose.model('User', UserSchema);
